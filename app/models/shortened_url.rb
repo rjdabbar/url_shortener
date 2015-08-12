2a8de2c48490d8bab1e2fd5e6e6e8c25adb55308
RJ & Edmund <RJ+Edmund@appacademy.io>
2015-08-12 14:40:54 -0400

    Add ShortenedURL::random_code and ShortenedURL::create_for_user_and_long_url! .

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -2,4 +2,15 @@ class ShortenedURL < ActiveRecord::Base
   validates :long_url, presence: true
   validates :short_url, presence: true, uniqueness: true
   validates :submitter_id, presence: true
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    new(submitter_id: user.id, long_url: long_url, short_url: random_code)
+  end
+
+  def self.random_code
+    loop do
+      random_url = SecureRandom.urlsafe_base64
+      return random_url unless exists?(short_url: random_url)
+    end
+  end
 end
